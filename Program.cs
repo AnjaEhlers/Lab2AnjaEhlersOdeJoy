6e9f44086aac3984de2a84fbd398a71e1d6d50fa
Anja Ehlers <AnjaEhlers.bittygurl11@gmail.com>
2022-09-18 17:02:19 -0600

    commit message

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿int a = 440;
+int b = 494;
+int c = 262;
+int d = 294;
+int e = 330;
+int f = 349;
+int g = 392;
+
+int wholeNote = 4000;
+int quarterNote = 1000;
+int halfNote = 2000;
+
+Console.Beep(e, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+//
+//
+//
+Console.Beep(e, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+//
+//
+//
+Console.Beep(d, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, 500);
+Console.Beep(f, 500);
+Console.Beep(e, 500);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, 500);
+Console.Beep(f, 500);
+Console.Beep(e, 500);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+//
+//
+//
+Console.Beep(e, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(g, quarterNote/2);
+Console.Beep(f, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(e, quarterNote/2);
+Console.Beep(d, quarterNote/2);
+Console.Beep(c, quarterNote/2);
+Console.Beep(c, quarterNote);
